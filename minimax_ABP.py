a12c16ac55cef0cdfdf622391bcb856602c6e7eb
hillyadler <Hilly@Hillys-MacBook-Pro.local>
2015-04-29 00:53:55 -0400

    well fuck

--- minimax_ABP.py
+++ minimax_ABP.py
@@ -38,9 +38,12 @@ class Minimax(object):
 """
 
 class node(object):
-    def __init__(self, value, children = []):
+    def __init__(self, value):
         self.value = value
-        self.children = children
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
 
 class Minimax(object):
     
@@ -48,25 +51,26 @@ class Minimax(object):
         self.state = state
         self.color = color
      
-    def make_children(self, state, color, depth):
-        if depth > 0:
-            children = [None] * 7            
+    def make_children(self, tree, state, color, depth):
+        if depth > 0:            
             new_states = [None] * 7            
             for i in range(7):
                 new_states[i] = state
-                for j in xrange(6):
-                    if new_states[i][j][i] == ' ':
-                        new_states[i][j][i] = color
+       #         for j in xrange(6):
+        #            if new_states[i][j][i] == ' ':
+         #               new_states[i][j][i] = color
             for i in range(7):
                 if color == "x":
                     color = "o"
                 else: color = "x"
-                children[i]=node(new_states[i], self.make_children(new_states[i], color, depth-1))
-            return children
+                currentTree = tree.add_child(node(new_states[i]))
+                self.make_children(currentTree.children[i], new_states[i], color, depth-1)
 
         
     def build_tree(self, state, color, depth):       
-        return node(state, self.make_children(state, color, depth))
+        tree = node(state)
+        self.make_children(tree, state, color, depth)
+        return tree
         
     def rank(self, state, color):
         rank = 0               
@@ -79,32 +83,32 @@ class Minimax(object):
             if colcount >= 4:
                 rank += 10                
 
-    def minimax(self, node, depth, color):
+    def minimax(self, node, depth, color):      
         if depth == 0: # or node is a terminal node
-            return self.rank(node[0])
+            return self.rank(node.value)
         if color == "o":
             bestValue = -10
             for i in xrange(7):
-                val = self.minimax(node[1][i], depth - 1, "x")
+                val = self.minimax(node.children[i], depth - 1, "x")
                 bestValue = max(bestValue, val)
             return bestValue
         else:
             bestValue = 10
             for i in xrange(7):
-                val = self.minimax(node[1][i], depth - 1, 'o')
+                val = self.minimax(node.children[i], depth - 1, 'o')
                 bestValue = min(bestValue, val)
             return bestValue
 
-    def bestestMove(self, node, color, depth):
+    def bestestMove(self, node, color, depth):     
         bestMove = None        
         bestValue = -10        
         for i in xrange(7):
-            currentval = self.minimax(node[1][i], depth, color)
+            currentval = self.minimax(node.children[i], depth, color)
             if currentval > bestValue:
                 bestValue = currentval
                 bestMove = i
         return bestMove
 
     def bestMove(self, difficulty, state, color):
-        tree = self.build_tree(state, color, difficulty)
-        return self.bestestMove(tree[0], color, difficulty)
+        tree = self.build_tree(state, color, difficulty)      
+        return self.bestestMove(tree, color, difficulty)
